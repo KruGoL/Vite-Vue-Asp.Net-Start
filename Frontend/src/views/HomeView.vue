10039766d20f35fddcddf3aabb31d9cc741a6384
Alexander Khrushkov <alexander.khrushkov@ttu.ee>
2023-03-17 17:31:09 +0200

    Init

---
+++ Frontend/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import TheWelcome from '../components/TheWelcome.vue'
+import axios from 'axios'
+
+const weather = ref([])
+
+const tags = async () => {
+  const resp = await axios.get('/WeatherForecast')
+  weather.value = resp.data
+  console.log(weather)
+}
+
+tags()
+</script>
+
+<template>
+  <main>
+    <h1>{{ weather[1].summary }}</h1>
+    <TheWelcome />
+  </main>
+</template>
